555d6ea724e983976c6b46e9507921096ab4940d
Olivier Buffon <olivierbuffon@gmail.com>
2018-09-05 16:32:00 -0400

    remove fake specs

--- spec/sisense_spec.rb
+++ spec/sisense_spec.rb
@@ -1,9 +1,3 @@
 RSpec.describe Sisense do
-  it 'has a version number' do
-    expect(Sisense::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  
 end
