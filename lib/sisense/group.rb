49e587041abf70c95fbcb3c4703519de926fb7a8
Olivier Buffon <olivierbuffon@gmail.com>
2018-09-23 16:46:12 -0400

    Add groups support to the gem

--- lib/sisense/group.rb
+++ lib/sisense/group.rb
@@ -2,9 +2,21 @@ module Sisense
   class Group < API::Resource
     RESOURCE_NAME = 'groups'.freeze
 
-    def self.list
+    def self.list(params: nil)
       path = resource_base_path
-      response = api_client.get(path)
+      response = api_client.get(path, params: params)
+      api_client.parsed_response(response, object_class: self)
+    end
+
+    def self.retrieve(id:, params: nil)
+      path = [resource_base_path, id].join('/')
+      response = api_client.get(path, params: params)
+      api_client.parsed_response(response, object_class: self)
+    end
+
+    def self.create(params:)
+      path = resource_base_path
+      response = api_client.post(path, params: params)
       api_client.parsed_response(response, object_class: self)
     end
   end
---
+++ spec/sisense/group_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe Sisense::Group do
+  it_behaves_like 'a sisense resource'
+  it_behaves_like 'a listable resource'
+  it_behaves_like 'a retrievable resource' do
+    let(:resource_id) { '5b3293ab0800930a10dd0d3b' }
+  end
+  it_behaves_like 'a creatable resource' do
+    let(:params) { { name: 'Test' } }
+  end
+end
